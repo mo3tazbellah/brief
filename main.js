14bc849f360b8d1459862912f305ad494d8e0d8b
moataz-bellah <82526990+mo3tazbellah@users.noreply.github.com>
2023-06-22 19:17:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,50 +1,18 @@
-headers = ['moataz', 'bellah', 'projects', 'brief']
-const h1 = document.querySelector('h1')
-const a = document.querySelector('.mail')
-let x = 0;
-let i = 0;
-let txt = header()
-const mail = `moatazbellah70@gmail.com`//24
-let mailIndex = 0;
-
-
-function typing() {
-    if (i < txt.length) {
-        h1.textContent += txt.charAt(i)
-        i++
-    } else {
-        i = 0;
-        txt = header()
-        h1.textContent = `${txt.charAt(i)}`
-        i++
+const projectsLinks = document.querySelectorAll('.container a')
+const projectsNum = document.querySelectorAll('.no')
+!(function () {
+    for(let i = 0; i<projectsLinks.length;i++){
+        projectsLinks[i].addEventListener('mouseenter',()=>{
+            projectsNum[i].innerHTML = ''
+            for(let hashIndex = 0 ; hashIndex < projectsLinks[i].textContent.length-2;hashIndex++){
+            projectsNum[i].textContent += '#'
+        }
+            setTimeout(()=>{projectsNum[i].innerHTML = projectsLinks[i].textContent},50)
+        })
+        projectsLinks[i].addEventListener('mouseleave',()=>{
+            projectsNum[i].innerHTML = ''
+            projectsNum[i].textContent = '###'
+            setTimeout(()=>{projectsNum[i].innerHTML = `0${i+1}/`},50)
+        })
     }
-
-    setTimeout(typing, 250)
-}
-typing()
-
-function header() {
-    let currHeader
-    if (x < headers.length) {
-        currHeader = headers[x]
-        x++;
-    } else {
-        x = 0;
-        currHeader = headers[x]
-        x++;
-    }
-    return currHeader;
-}
-
-function mailType() {
-    if (mailIndex < mail.length) {
-        a.textContent += mail.charAt(mailIndex)
-        mailIndex++
-       setTimeout(mailType,75)
-    }
-}
-mailType();
-
-a.addEventListener('click',()=>{
-    document.querySelector('audio').play()
-})
+})();
